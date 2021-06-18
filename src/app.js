2a4a099ff190dd1e254544f69acee014905dcb1b
Felipe Massao Kitanaka Matsuoka <massao.felipe@gmail.com>
2021-06-17 22:10:37 -0300

    padronizando rotas do frontend

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require("cors");
 require('dotenv').config()
-const cors = require('cors');
 
 const app = express();
 
