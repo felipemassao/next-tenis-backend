d51cc0739f0daaf2b54e844f5c099e1ea89cf06d
William Quirino Ferreira <wqferreira@gmail.com>
2021-06-29 21:39:10 -0300

    ajustes manutencao

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const controller = require('../controllers/users');
 
 router.get('/', controller.listarUsuarios);
+router.get('/:id', controller.buscaUsuario);
+router.put('/:id', controller.atualizarUsuario);
 router.post('/login', controller.entrar);
 router.post('/cadastrar', controller.checkDupNameEmail, controller.novoCadastro);
+router.delete('/:id', controller.deletarUsuario);
 
 module.exports = router;
